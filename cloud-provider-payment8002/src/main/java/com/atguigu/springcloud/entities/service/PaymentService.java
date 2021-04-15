03c52ca462600f431c1be2fb5c13a97eb7c2c140
2119299531@qq.com <2119299531@qq.com>
2021-04-15 22:32:18 +0800

    '6'

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/entities/service/PaymentService.java
+++
@@ -1,9 +0,0 @@
-//PaymentService
-package com.atguigu.springcloud.entities.service;
-import com.atguigu.springcloud.entities.Payment;
-import org.apache.ibatis.annotations.Param;
-public interface PaymentService {
-    public int create(Payment payment);
-
-    public Payment getPaymentById(@Param("id") Long id);
-}
