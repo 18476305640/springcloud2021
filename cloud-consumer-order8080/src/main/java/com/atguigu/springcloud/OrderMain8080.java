e9b3db47e4d9ffbd01db86974312c3c7dbbcb396
2119299531@qq.com <2119299531@qq.com>
2021-04-17 16:37:30 +0800

    '解决了问题'

--- cloud-consumer-order8080/src/main/java/com/atguigu/springcloud/OrderMain8080.java
+++ cloud-consumer-order8080/src/main/java/com/atguigu/springcloud/OrderMain8080.java
@@ -1,7 +1,10 @@
 package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain8080 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain8080.class,args);
