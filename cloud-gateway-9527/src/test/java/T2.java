4e0eeaa7cbb986c56cf822653c21156989596122
2119299531@qq.com <2119299531@qq.com>
2021-05-04 12:11:46 +0800

    'Gateway断言完成'

---
+++ cloud-gateway-9527/src/test/java/T2.java
@@ -0,0 +1,10 @@
+import com.thoughtworks.xstream.converters.time.ZonedDateTimeConverter;
+
+import java.time.ZonedDateTime;
+
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zdt = ZonedDateTime.now();
+        System.out.println(zdt);
+    }
+}
